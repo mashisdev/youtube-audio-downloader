c33733dc507a3157c697eea9391802793f58698c
manuelsrl <manuelschallerrosal@gmail.com>
2025-09-09 20:29:29 -0300

    1º commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import yt_dlp
+import os
+
+def download_audio(url: str):
+    downloads_folder = os.path.join(os.path.dirname(__file__), "downloads")
+    os.makedirs(downloads_folder, exist_ok=True)
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": os.path.join(downloads_folder, "%(title)s.%(ext)s"),
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "ffmpeg_location": "C:/ffmpeg/bin"
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+if __name__ == "__main__":
+    video_url = str(input("Enter the YouTube video URL: ")).strip()
+    download_audio(video_url)
+    print("Download completed! File saved in the 'downloads' folder.")
